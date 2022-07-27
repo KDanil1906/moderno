993ef2bedb1e7bd073cf1eba650420fba9a4c9fa
Danil <danilkozlov36@yandex.ru>
2022-07-27 23:39:36 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -17,6 +17,31 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   $('.menu__btn').on('click', function () {
